31474b20352dc2b084b50737c2cce3f4f4c52915
bao <1730882047@qq.com>
2019-12-24 16:55:20 +0800

    AppBarLayout

--- app/src/main/java/com/example/baopengjian/ray_seniorui/thirtieth/AppbarLayoutActivity.java
+++ app/src/main/java/com/example/baopengjian/ray_seniorui/thirtieth/AppbarLayoutActivity.java
@@ -1,11 +1,7 @@
 package com.example.baopengjian.ray_seniorui.thirtieth;
 
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.example.baopengjian.ray_seniorui.R;
 
@@ -14,32 +10,10 @@ import com.example.baopengjian.ray_seniorui.R;
  */
 public class AppbarLayoutActivity extends AppCompatActivity {
 
-    private RecyclerView mRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.app_bar_layout_activity);
-       // initDate();
-        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayShowCustomEnabled(true);
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressed();
-            }
-        });
-        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collasing);
-        collapsingToolbarLayout.setTitle("AppbarLayout");
     }
-  /*  private void initDate(){
-        mRecyclerView = (RecyclerView) this.findViewById(R.id.recyclerView);
-        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
-        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-        List<Integer> datas = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            datas.add(i);
-        }
-        mRecyclerView.setAdapter(new MyRecyclerViewAdapter(this, datas));
-    }*/
+
 }
